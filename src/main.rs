f210104745bb54725af8508acda037f1cf703d90
Nyakio Maina <nyakiomaina@Nyakios-machine.local>
2024-05-08 15:53:26 +0300

    generate output paths

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use image::{ImageError, DynamicImage, io::Reader as ImageReader};
 use rayon::prelude::*;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 fn load_image<P>(path: P) -> Result<DynamicImage, ImageError>
 where
@@ -10,8 +10,16 @@ where
     Ok(img)
 }
 
-fn save_image(img: &DynamicImage, path: &str) -> Result<(), ImageError> {
+fn save_image(img: &DynamicImage, path: &Path) -> Result<(), ImageError> {
     img.save(path)
+}
+
+fn output_path(input_path: &Path, desc: &str) -> PathBuf {
+    let mut path  = input_path.to_path_buf();
+    let stem = path.file_stem().unwrap().to_str().unwrap();
+    let extension = path.extension().unwrap().to_str().unwrap();
+    path.set_file_name(format!("{}_{}.{}", stem, desc, extension));
+    path
 }
 
 fn grayscale(img: &DynamicImage) -> DynamicImage {
@@ -29,19 +37,21 @@ fn brighten(img: &DynamicImage) -> DynamicImage {
 }
 
 fn main() {
-    let input_path = "images/cat1.jpg";
-    let output_gray_path = "images/cat_output.jpg";
-    let output_bright_path = "images/cat_bright.jpg";
+    let input_path: &str = "images/cat1.jpg";
+    let input_path = Path::new(input_path);
+
+    let output_gray_path = output_path(input_path, "gray");
+    let output_bright_path = output_path(input_path, "bright");
 
     match load_image(input_path) {
         Ok(img) => {
             let gray_img = grayscale(&img);
-            if let Err(e) = save_image(&gray_img, output_gray_path) {
+            if let Err(e) = save_image(&gray_img, &output_gray_path) {
                 eprintln!("failed to save image: {}", e);
             }
 
             let bright_img = brighten(&img);
-            if let Err(e) = save_image(&bright_img, output_bright_path) {
+            if let Err(e) = save_image(&bright_img, &output_bright_path) {
                 eprintln!("Failed to save brightened image: {}", e);
             }
         },
